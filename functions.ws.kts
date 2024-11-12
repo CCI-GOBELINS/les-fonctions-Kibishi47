afafc0cd953707a707461359a17cb96a4bb491d8
Emmanuel Moulin <78955048+Kibishi47@users.noreply.github.com>
2024-11-12 11:44:41 +0100

    feat: get prime numbers

--- functions.ws.kts
+++ functions.ws.kts
@@ -35,9 +35,10 @@ println(getNumbers(10, isEven = false))
 fun getFibonacciNumbers(x: Int): List<Int> {
     if (x > 0) {
         var numbers = mutableListOf<Int>()
-        var startingNumber = 0
         for (i in 0..x-1) {
-            if (numbers.count() == 0 || numbers.count() == 1) {
+            if (numbers.count() == 0) {
+                numbers.add(0)
+            } else if (numbers.count() == 1) {
                 numbers.add(1)
             } else {
                 numbers.add(numbers[i-2] + numbers[i-1])
@@ -61,3 +62,34 @@ fun factorial(x: Int = 10): Int {
 }
 
 println(factorial(5))
+
+fun isPrimeNumber(x: Int): Boolean {
+    if (x > 1) {
+        for (i in x - 1 downTo 2) {
+            if (x % i == 0){
+                return false
+            }
+        }
+        return true
+    }
+    return false
+}
+
+fun getPrimeNumbers(x: Int): List<Int> {
+    if (x > 0) {
+        var numbers = mutableListOf<Int>()
+        var i = 0
+        while (numbers.count() < x) {
+            if (isPrimeNumber(i)){
+                numbers.add(i)
+            }
+            i++
+        }
+        return numbers
+    } else {
+        println("You can not get a number of amount of 0 or less")
+        return listOf<Int>()
+    }
+}
+
+println(getPrimeNumbers(10))
